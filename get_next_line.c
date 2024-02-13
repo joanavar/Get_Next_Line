b9faea0370a590daf1931d3bab72c0c8ad42387d
Joan Navarro Gomez <joanavar@car12s4.42barcelona.com>
2024-02-13 18:22:10 +0100

    Empezando get next line

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,26 @@
 /*   By: joanavar <joanavar@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 14:49:47 by joanavar          #+#    #+#             */
-/*   Updated: 2024/02/13 15:38:57 by joanavar         ###   ########.fr       */
+/*   Updated: 2024/02/13 18:20:55 by joanavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 char *get_next_line(int fd);
+{
+	static char		*lectur;
+	int				count;
+	int 			i;
+	char			*tmp;
+// while(mientras fd exista)
+// tengo que ir leyendo linea por linea, ir aplicando calloc a cada linea para tener el espacio justo para escribirla y que no sobre espacio de memoria
+//		while(str[i] != '\n' || str[i] != '\0')
+//			count++;
+//		tmp = calloc(sizeof(char) * BUFFER_SIZE + 1)
+
+
+
 
 int main(void)
 {
